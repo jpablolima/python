f431f9ad5a4dad3d117aa8110dfa5fc755a4ff11
João Pablo <jp.pablolima@gmail.com>
2019-11-19 12:25:29 -0300

    classes e objetos

---
+++ classes_objetos.py
@@ -0,0 +1,14 @@
+class Cliente:
+  def __init__(self, nome, telefone):
+    self.nome = nome
+    self.telefone = telefone
+    goku=Cliente("Son Goku", "777-12345")
+    vegeta=Cliente("Vegeta table", "777-54321")
+    goku.nome
+    goku.telefone
+    vegeta.nome
+    vegeta.telefone   
+    print(goku.nome)
+    print(goku.telefone)
+    print(vegeta.nome)
+    print(vegeta.telefone)
---
+++ teste.py
@@ -0,0 +1,3 @@
+from  clientes import CLiente
+goku=Cliente("Son Goku", "777-12345")
+vegeta=Cliente("Vegeta table", "777-54321")
